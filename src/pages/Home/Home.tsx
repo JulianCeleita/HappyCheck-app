2c1b2a63224bfa0777aa0cefa3d7434efe8bd9a5
Julian Celeita <andresceleita@yahoo.es>
2023-01-30 22:05:15 -0500

    columns configured

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,26 +5,48 @@ import { People } from "@/data/people";
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
-	const pageSize = 5;
-	const columns = [{
-		field: "name",
-		headerName: "Name",
-		flex: 1,
-		minWidth: 150,
-		renderCell: (params: GridRenderCellParams)=> <>{params.field} </> 
-	}]
+  const pageSize = 5;
+  const columns = [
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value} </>,
+    },
+    {
+      field: "category",
+      headerName: "Categories",
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value} </>,
+    },
+    {
+      field: "company",
+      headerName: "Company",
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value} </>,
+    },
+    {
+      field: "levelOfHappiness",
+      headerName: "Happines",
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value} </>,
+    },
+  ];
   return (
-    <div>
-      <DataGrid
-	  	rows={People}
-		columns={columns}
-        disableColumnSelector
-		disableSelectionOnClick
-		autoHeight
-		pageSize={pageSize}
-		rowsPerPageOptions={[pageSize]}
-      />
-    </div>
+    <DataGrid
+      rows={People}
+      columns={columns}
+      disableColumnSelector
+      disableSelectionOnClick
+      autoHeight
+      pageSize={pageSize}
+      rowsPerPageOptions={[pageSize]}
+      getRowId={(row: any) => row.id}
+    />
   );
 };
 
