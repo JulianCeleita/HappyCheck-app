dc0a6434d23cae9f5caafe058b2e355f433c9005
Julian Celeita <andresceleita@yahoo.es>
2023-01-30 21:14:41 -0500

    Implement Datagrid and navbar modified

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React from "react";
+import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid";
+import { People } from "@/data/people";
+
 export interface HomeInterface {}
 
-const Home : React.FC<HomeInterface> = () => {
-	return <div>Home</div>;
+const Home: React.FC<HomeInterface> = () => {
+	const pageSize = 5;
+	const columns = [{
+		field: "name",
+		headerName: "Name",
+		flex: 1,
+		minWidth: 150,
+		renderCell: (params: GridRenderCellParams)=> <>{params.field} </> 
+	}]
+  return (
+    <div>
+      <DataGrid
+	  	rows={People}
+		columns={columns}
+        disableColumnSelector
+		disableSelectionOnClick
+		autoHeight
+		pageSize={pageSize}
+		rowsPerPageOptions={[pageSize]}
+      />
+    </div>
+  );
 };
 
 export default Home;
