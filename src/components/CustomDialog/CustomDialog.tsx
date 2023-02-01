2de20a73fa498ded0314fa265fe7f1b55b5f0f69
Julian Celeita <andresceleita@yahoo.es>
2023-01-31 22:37:42 -0500

    New subject manager, icon checked

---
+++ src/components/CustomDialog/CustomDialog.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export interface CustomDialogInterface {}
+
+const CustomDialog : React.FC<CustomDialogInterface> = () => {
+	return <div>CustomDialog</div>;
+};
+
+export default CustomDialog;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,29 @@
-import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
 import React from "react";
+import { CustomDialog } from "../CustomDialog";
+import FavoriteTable from "./FavoriteTable/FavoriteTable";
+import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
 
 export interface NavbarInterface {}
 
 const Navbar: React.FC<NavbarInterface> = () => {
   return (
-    <div>
+    <>
+      <CustomDialog>
+        <FavoriteTable />
+      </CustomDialog>
       <AppBar color="secondary" position="fixed">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Happy Check Application
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button size="large" sx={{ color: "white" }}
+        startIcon={<AssignmentTurnedInIcon/>}
+      > Checked
+      </Button>
         </Toolbar>
       </AppBar>
-    </div>
+    </>
   );
 };
 
